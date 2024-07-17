c4b317b9bdd6d176f3a399b78a23b8546bbdca6a
livia.pereira <liviap520@gmail.com>
2024-07-16 22:01:17 -0300

    feat: cria o projeto java com o SpringInitializer

---
+++ chain-care/src/main/java/com/chain_care/chain_care/ChainCareApplication.java
@@ -0,0 +1,13 @@
+package com.chain_care.chain_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainCareApplication.class, args);
+	}
+
+}
---
+++ chain-care/src/test/java/com/chain_care/chain_care/ChainCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chain_care.chain_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
