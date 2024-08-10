1dc02b0f8aec760c633261f9fbc70cf009814c08
livia.pereira <liviap520@gmail.com>
2024-08-10 14:49:50 -0300

    fix: corrige atributos do model Paciente

--- chaincare/src/main/java/com/chaincare/chaincare/model/Paciente.java
+++ chaincare/src/main/java/com/chaincare/chaincare/model/Paciente.java
@@ -26,12 +26,12 @@ public class Paciente {
 
     @Size(max = 20)
     @NotNull
-    @Column(name = "cns", nullable = false, length = 20)
+    @Column(name = "cns", unique = true, nullable = false, length = 20)
     private String cns;
 
     @Size(max = 100)
     @NotNull
-    @Column(name = "nome", nullable = false, unique = true, length = 100)
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 
     @Size(max = 255)
@@ -50,22 +50,22 @@ public class Paciente {
 
     @NotNull
     @Size(max = 20)
-    @Column(name = "telefone", nullable = false, length = 20)
+    @Column(name = "telefone", length = 20)
     private String telefone;
 
     @NotNull
-    @Column(name = "data_nasc", nullable = false)
-    private LocalDate dataNasc;
-
-    @NotNull
-    @Column(name = "sexo", nullable = false, length = 1, columnDefinition = "CHECK (Sexo IN ('M', 'F'))")
+    @Column(name = "sexo", length = 1, columnDefinition = "CHECK (Sexo IN ('M', 'F'))")
     private Character sexo;
 
+    @NotNull
+    @Column(name = "data_nasc")
+    private LocalDate dataNasc;
+
     @Size(max = 50)
-    @Column(name = "status", nullable = false, length = 50)
+    @Column(name = "status", length = 50)
     private String status;
 
     @Size(max = 100)
-    @Column(name = "convenio", nullable = false, length = 100)
+    @Column(name = "convenio", length = 100)
     private String convenio;
 }
