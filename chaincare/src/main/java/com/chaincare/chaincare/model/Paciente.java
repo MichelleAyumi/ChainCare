54f2e062e28725106efcd65fc746e2d299039c7c
livia.pereira <liviap520@gmail.com>
2024-08-14 21:11:01 -0300

    refactor: adiciona id padrão

--- chaincare/src/main/java/com/chaincare/chaincare/model/Paciente.java
+++ chaincare/src/main/java/com/chaincare/chaincare/model/Paciente.java
@@ -21,7 +21,7 @@ public class Paciente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     @Column(name = "id")
-    private Integer id;
+    private Integer id = 0;
 
     @Size(max = 20)
     @NotNull
